db709f58dd029d5b12ba5b7b04553330249e1365
Alex Lemenze <alemenze@gmail.com>
2026-05-22 23:01:48 -0400

    GA fix

--- app/ui.R
+++ app/ui.R
@@ -21,7 +21,7 @@ library(msigdbr)
 
 tagList(
     tags$head(
-        #includeHTML(("www/GA.html")),
+        includeHTML(("www/GA.html")),
         tags$style(type = 'text/css','.navbar-brand{display:none;}'),
         tags$style(HTML("
             .control-group-panel {
